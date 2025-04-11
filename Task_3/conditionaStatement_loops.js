91f680747bd9951d4bc61c8e56d2dcec550c848f
Lwando Sodinga <125615819+LwandoS30@users.noreply.github.com>
2025-04-11 15:15:50 +0200

    Update conditionaStatement_loops.js

--- Task_3/conditionaStatement_loops.js
+++ Task_3/conditionaStatement_loops.js
@@ -21,14 +21,67 @@ else
 }
 
 //2)
-console.log("===================================================")
+switch(tempreture)
+{
+    case "freezing":
+        console.log("It's freezoing");
+        break;
 
+    case "cold":
+        console.log("It's cold");
+        break;
+
+    case "mild":
+        console.log("It's mild");
+        break;
+
+    case "warm":
+        console.log("It's warm");
+        break;
+    default:
+        console.log("");
+        break;
+}
+        
+
+console.log("===================================================")
+//Exercise 2
 let num = 10
 
-if(num % 2 == 0)
+if(num % 2 == 0 && num % 3 == 0)
 {
     console.log("Divisible bt 2 and 3");
 }
+else if(num % 2 == 0)
+{
+    console.log("Divisible by 2");
+}
+else if(num % 3 == 0)
+{
+    console.log("Divisible by 3");
+}
+else
+{
+    console.log("Not divisible by 2");
+}
+
+console.log("===================================================")
+
+//Switch
+switch (true) {
+    case (num % 2 === 0 && num % 3 === 0):
+        console.log("Divisible by 2 and 3");
+        break;
+    case (num % 2 === 0):
+        console.log("Divisible by 2");
+        break;
+    case (num % 3 === 0):
+        console.log("Divisible by 3");
+        break;
+    default:
+        console.log("Not divisible by 2");
+}
+
 console.log("===================================================")
 
 //Exercise 3
@@ -53,23 +106,24 @@ console.log("===================================================")
 let sumResults = 0
 for(var c = 1; c <= 100; c++)
 {
-    console.log(c)
     sumResults += c
     
 }
 console.log(sumResults);
 
 console.log("===================================================")
+//4)
 const numbers = [1,2,3,4,5]
 for(var q = 0; q < numbers.length; q++)
 {
     console.log(numbers[q]);
 }
 
-
 console.log("===================================================")
+//5)
 const numberz = [3,7,2,5,10,6]
 let largest = 0
+
 for(var o = 0; o < numberz.length; o++)
 {
     if(numberz[o] > largest)
@@ -81,14 +135,14 @@ for(var o = 0; o < numberz.length; o++)
 console.log("===================================================")
 
 //Exercise 4
-//2)
+//1)
 let l = 1
 while(l <= 10)
 {
     console.log(l);
     l += 1;
 }
-//3)
+//2)
 
 console.log("===================================================")
 let w = 1
@@ -101,13 +155,53 @@ while(w <= 20)
     w += 1
 }
 console.log("===================================================")
-
+//4)
 let r = 1
 let sum100 = 0
 while(r <= 100)
 {
-    console.log(r)
     sum100 += r
 }
 console.log(sum100);
 console.log("===================================================")
+//Exercise 5
+//1)
+let k = 1
+
+do 
+{
+    console.log(k);
+    k += 1;
+}
+while(k == 10);
+
+//2)
+let g = 1
+let sumDo100 = 0
+do 
+{
+    g += 1;
+    sumDo100 += g
+}
+while(k == 100);
+console.log(sumDo100);
+
+//3)
+
+let guessNum = 0
+do
+{
+    guessNum = prompt("Please enter a number greater than 10: ");
+    
+}
+while(guessNum <= 10);
+
+//4)
+
+let equalNum = 0
+do
+{
+    equalNum = prompt("Please enter any number between 0 and 20")
+    randomNum = Math.Floor(Math.random() * 20) + 1;
+}
+while(equalNum == randomNum);
